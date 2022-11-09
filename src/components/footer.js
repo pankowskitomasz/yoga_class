d88f3076671fec6fa9ea5ae7fd97653f5c6bf9e0
TP <pankowski.tomasz@prokonto.pl>
2022-11-09 18:23:45 +0100

    gpdr, fonts, og

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,7 +12,7 @@ class Footer extends Component{
                     <Row className="mx-auto text-center d-flex w-100">
                         <Col xs={10} className="mx-auto">
                             <small className="my-0 text-caption">
-                                Copyright &copy; 2021 Tomasz Pankowski. All rights reserved. 
+                                Copyright &copy; 2021-2022 Tomasz Pankowski. All rights reserved. 
                                 <Link to={this.props.privacyLink.href} className="text-caption text-decoration-none">
                                     {this.props.privacyLink.name}
                                 </Link>
--- src/components/home-data.js
+++ src/components/home-data.js
@@ -16,14 +16,14 @@ class HomeData extends Component{
             </Link>;
         }
         return(        
-            <Container fluid className={"contact-data align-items-center p-0 d-flex minh-50vh "+this.props.classExt}>
+            <Container fluid className={"contact-data align-items-center p-0 d-flex minh-100vh "+this.props.classExt}>
                 <Row className="mx-auto text-center w-100 pt-5">
-                    <Col xs={10} md={6} className="mx-auto text-dark p-0 order-2 order-md-1">
+                    <Col xs={10} lg={6} className="mx-auto text-dark p-0 order-2 order-md-1">
                         <img alt="about"
                             className="img-fluid"
                             src="img/contact/contact-data.png"/>
                     </Col>
-                    <Col xs={10} md={6} className="mx-auto text-dark p-0 d-flex order-1 order-md-2 align-items-center">
+                    <Col xs={10} lg={6} className="mx-auto text-dark p-0 d-flex order-1 order-md-2 align-items-center">
                         <div className="p-5 contact-bg">
                             <h1 className="display-5 font-logo text-caption fw-bold">
                                 Yoga Class
